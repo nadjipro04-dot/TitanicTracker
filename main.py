13437e1089ca864e51fdf71aea290e09185e7515
nadjipro04-dot <nadjipro04@gmail.com>
2026-06-19 18:33:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import discord
+from discord.ext import commands
+import os
 
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.all()
+
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
+
+@bot.event
+async def on_ready():
+    print("Connecté :", bot.user)
+
+bot.run(TOKEN)
